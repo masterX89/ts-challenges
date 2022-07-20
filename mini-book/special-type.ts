98f7d0215b82acd6f85aa2bb61821f2ab4fdf9df
masterX89 <xhk1995@gmail.com>
2022-07-20 16:06:44 +0800

    feat: isTuple UnionToIntersection GetOptional GetRequest

--- mini-book/special-type.ts
+++ mini-book/special-type.ts
@@ -15,6 +15,8 @@ type IsEqualEnhance<A, B> = (<T>() => T extends A ? 1 : 2) extends <
 >() => T extends B ? 1 : 2
   ? true
   : false
+type isEqualEnhanceRes1 = IsEqualEnhance<any, 'A'> // will judge whether is any
+type isEqualEnhanceRes2 = IsEqualEnhance<any, any>
 
 // isUnion
 // A extends A 触发分布式
@@ -28,3 +30,40 @@ type isNever<T> = [T] extends [never] ? true : false
 // type isNever<T> = T extends never ? true : false // never
 type isNeverRes1 = isNever<never>
 type isNeverRes2 = isNever<1>
+
+// isTuple
+// Tuple 两个特征: 1. 元组 El 是 readonly 2. 元组 length 类型是数字字面量 数组 length 类型是 number
+type IsTuple<T> = T extends readonly [...params: infer Els]
+  ? NotEqual<Els['length'], number>
+  : false
+type NotEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends <
+  T
+>() => T extends B ? 1 : 2
+  ? false
+  : true
+type IsTupleRes1 = IsTuple<[1, 2, 3]>
+type IsTupleRes2 = IsTuple<number[]>
+
+// TODO: 逆变 协变
+// UnionToIntersection 逆变一般只用于此处
+type UnionToIntersectionStep1<U> = U extends U ? (x: U) => unknown : never
+type UnionToIntersection<U> = (
+  U extends U ? (x: U) => unknown : never
+) extends (x: infer R) => unknown
+  ? R
+  : never
+type UnionToIntersectionStep1Res = UnionToIntersectionStep1<
+  { x: number } | { y: string }
+>
+type UnionToIntersectionRes = UnionToIntersection<{ x: number } | { y: string }>
+
+// GetOptional
+type GetOptional<Obj extends Record<string, any>> = {
+  [Key in keyof Obj as {} extends Pick<Obj, Key> ? Key : never]: Obj[Key]
+}
+type GetOptionalRes = GetOptional<{ name: string; age?: number }>
+// GetRequest
+type GetRequest<Obj extends Record<string, any>> = {
+  [Key in keyof Obj as {} extends Pick<Obj, Key> ? never : Key]: Obj[Key]
+}
+type GetRequestRes = GetRequest<{ name: string; age?: number }>
